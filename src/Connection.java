5bc5255a53ce2ee48d5be68b4405a4b5923e819e
Trixi Turny <trixiturny@trixis-mbp.home>
2017-09-15 19:54:01 +0100

    classes + pseudo logic

--- src/Connection.java
+++ src/Connection.java
@@ -1,4 +1,24 @@
 
 public class Connection {
+	
+	private DefaultResponse resp;
+	
+	
+	public static void main(String[] args){
+		
+		//prompt user to enter urls 
+		//take urls then enter empty line or work this out somehow
+		//collect url in a list
+		//while there are urls
+		//when no more urls - for each url in list validate url -if invalid ->errorMap if valid-> collect data and put it in success/errorMap
+		//List of DefaultResponse Objects to json-> print out json
+		
+		
+		
+		
+		
+	}
+	
+	
 
 }
--- src/ConnectionTest.java
+++ src/ConnectionTest.java
@@ -1,4 +1,13 @@
 
 public class ConnectionTest {
+	
+	public static void main(String[] args){
+		
+		//pre-defined urls in a list
+		//call method of connection to determine if urls are valid
+		//output statuses 
+		
+		
+	}
 
 }
