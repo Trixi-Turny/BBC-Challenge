70c8e42a3146e554af6c43f4c660f5fe78331e5d
Trixi Turny <trixiturny@Trixis-MacBook-Pro.local>
2017-09-16 08:02:49 +0100

    Read in multiline urls, add them to List

--- src/Connection.java
+++ src/Connection.java
@@ -1,24 +1,29 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Connection {
-	
-	private DefaultResponse resp;
-	
-	
-	public static void main(String[] args){
-		
-		//prompt user to enter urls 
-		//take urls then enter empty line or work this out somehow
-		//collect url in a list
-		//while there are urls
-		//when no more urls - for each url in list validate url -if invalid ->errorMap if valid-> collect data and put it in success/errorMap
-		//List of DefaultResponse Objects to json-> print out json
-		
-		
-		
-		
-		
-	}
-	
-	
 
+	private DefaultResponse resp;
+
+	public static void main(String[] args) {
+		ArrayList<String> urls = new ArrayList<String>();
+		boolean noMoreUrls = false;
+		String url;
+
+		try (Scanner scanner = new Scanner(System.in)) {
+			System.out.println("Please enter the urls:\nInput Stream will finish when you press enter twice.");
+			do{
+				url  = scanner.nextLine();
+				if(! url.equals("")){
+					urls.add(url);	
+				}
+				else{
+					break;
+				}
+					
+			}while(scanner.hasNextLine());
+			
+		System.out.println(urls.toString());
+		}
+	}
 }
